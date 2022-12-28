5172e041438bad931faef1830866af03eb6f671b
claudio-guiavet <claudio@guia.vet>
2022-12-28 20:37:56 -0300

    feat: add useSummary hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,32 +1,14 @@
-import { useContext } from "react";
-
 import { useTheme } from "styled-components";
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 
+import { useSummary } from "../../hooks/useSummary";
 import { priceFormatter } from "../../utils/formatter";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 import { SummaryCard, SummaryContainer } from "./styles";
 
 export function Summary() {
   const theme = useTheme();
-  const { transactions } = useContext(TransactionsContext);
-
-  const summary = transactions.reduce(
-    (acc, current) => {
-      if (current.type === "income") {
-        acc.income += current.price;
-        acc.total += current.price;
-      }
-      if (current.type === "outcome") {
-        acc.outcome += current.price;
-        acc.total -= current.price;
-      }
-
-      return acc;
-    },
-    { income: 0, outcome: 0, total: 0 }
-  );
+  const summary = useSummary()
 
   return (
     <SummaryContainer>
---
+++ src/hooks/useSummary.tsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+
+import { TransactionsContext } from "../contexts/TransactionsContext";
+
+export function useSummary() {
+  const { transactions } = useContext(TransactionsContext);
+
+  const summary = transactions.reduce(
+    (acc, current) => {
+      if (current.type === "income") {
+        acc.income += current.price;
+        acc.total += current.price;
+      }
+      if (current.type === "outcome") {
+        acc.outcome += current.price;
+        acc.total -= current.price;
+      }
+
+      return acc;
+    },
+    { income: 0, outcome: 0, total: 0 }
+  );
+
+  return summary;
+}
