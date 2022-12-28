206d44c047ff375c3f9a1804b7b271bf9c7a92a1
claudio-guiavet <claudio@guia.vet>
2022-12-27 23:56:53 -0300

    feat: add TransactionsTable

---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,43 @@
+import styled, { css } from "styled-components";
+
+export const SummaryContainer = styled.section`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+
+  margin-top: -5rem;
+`;
+
+interface SummaryCardProps {
+  variant?: "green";
+}
+
+export const SummaryCard = styled.div<SummaryCardProps>`
+  background: ${({ theme }) => theme["gray-600"]};
+  border-radius: 6px;
+  padding: 2rem;
+
+  header {
+    align-items: center;
+    color: ${({ theme }) => theme["gray-300"]};
+    display: flex;
+    justify-content: space-between;
+  }
+
+  strong {
+    display: block;
+    font-size: 2rem;
+    margin-top: 1rem;
+  }
+
+  ${({ theme, variant }) =>
+    variant === "green" &&
+    css`
+      background: ${theme["green-700"]};
+    `}
+`;
--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -1,3 +1,40 @@
 import styled from "styled-components";
 
-export const TransactionsContainer = styled.div``;
+export const TransactionsContainer = styled.main`
+  margin: 4rem auto 0;
+  max-width: 1120px;
+  padding: 0 1.5rem;
+  width: 100%;
+`;
+
+export const TransactionsTable = styled.table`
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+  margin-top: 1.5rem;
+  width: 100%;
+
+  td {
+    background: ${(props) => props.theme["gray-700"]};
+    padding: 1.25rem 2rem;
+
+    &:first-child {
+      border-bottom-left-radius: 6px;
+      border-top-left-radius: 6px;
+    }
+
+    &:last-child {
+      border-bottom-right-radius: 6px;
+      border-top-right-radius: 6px;
+    }
+  }
+`;
+
+interface PriceHighlightProps {
+  variant?: "income" | "outcome";
+}
+
+export const PriceHighlight = styled.span<PriceHighlightProps>`
+  color: ${({ theme, variant }) =>
+    variant === "income" ? theme["green-300"] : theme["red-300"]};
+  
+`;
