2ef39377a1fbfc974ffbadfc2ec76ea29a5b6140
claudio-guiavet <claudio@guia.vet>
2022-12-28 23:11:37 -0300

    feat: add axios

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 
+import { api } from "../lib/axios";
 import { TransactionProps } from "../pages/Transactions";
 
 interface TransactionsContextProps {
@@ -19,15 +20,9 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<TransactionProps[]>([]);
 
   async function fetchTransactions(query?: string) {
-    const url = new URL("http://localhost:3000/transactions");
+    const response = await api.get("/transactions", { params: { q: query } });
 
-    if (query) url.searchParams.append("q", query);
-
-    const response = await fetch(url);
-
-    const data = await response.json();
-
-    setTransactions(data);
+    setTransactions(response.data);
   }
 
   useEffect(() => {
